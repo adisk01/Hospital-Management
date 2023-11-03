2ae1643582c4bfdfd40a820ce1da3221c350bce7
adisk01 <adiscrazy01@gmail.com>
2023-11-04 02:02:01 +0530

    backend done

---
+++ client/src/pages/layout.js
@@ -0,0 +1,9 @@
+class days {
+    public static void main(string args[])
+    {
+        Thread t=Thread.currentThread();
+        t.setNAme("mon");
+        t.setNAme("mon");
+        SVGPolygonElement;
+    }
+}
